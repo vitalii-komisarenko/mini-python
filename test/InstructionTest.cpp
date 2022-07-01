0a8ae1394b7dbdbee79be43e621de0ffafcc6a90
Vitalii Komisarenko <vitalii.komisarenko@gmail.com>
2022-07-02 00:17:02 +0200

    Instruction test: add macros to check VAR_NAME and RET_VALUE instructions

--- test/InstructionTest.cpp
+++ test/InstructionTest.cpp
@@ -2,6 +2,17 @@
 
 #include "Test.h"
 
+#define ASSERT_IS_VAR(param, varname) \
+    MY_ASSERT(param->op == Operation::VAR_NAME); \
+    MY_ASSERT(param->var != nullptr); \
+    MY_ASSERT(param->var->get_type() == VariableType::STRING); \
+    MY_ASSERT(std::dynamic_pointer_cast<StringVariable>(param->var)->get_value() == varname);
+
+#define ASSERT_IS_VALUE(param, TYPE1, TYPE2, VALUE) \
+    MY_ASSERT(param->op == Operation::RET_VALUE); \
+    MY_ASSERT(param->var != nullptr); \
+    CHECK_VAR(param->var, TYPE1, TYPE2, VALUE);
+
 void test_instruction() {
     // a = 3
     TokenList simpleAssignmentTokens = {
@@ -17,6 +28,8 @@ void test_instruction() {
 
     if (simpleAssignmentInstruction.params.size() == 2) {
         MY_ASSERT(simpleAssignmentInstruction.params[0]->op == Operation::VAR_NAME);
+        ASSERT_IS_VAR(simpleAssignmentInstruction.params[0], "a");
         MY_ASSERT(simpleAssignmentInstruction.params[1]->op == Operation::RET_VALUE);
+        ASSERT_IS_VALUE(simpleAssignmentInstruction.params[1], INT, Int, 3);
     }
 }

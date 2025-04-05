e00570d046e9bb7cf41658b22768cc76751d475b
Vitalii Komisarenko <vitalii.komisarenko@gmail.com>
2025-04-05 07:37:07 +0200

    TokenToVariable: Support numbers: oct, hex and notation with underscores

--- src/TokenToVariable.cpp
+++ src/TokenToVariable.cpp
@@ -17,11 +17,22 @@ Variable parseTokenToVariable(const Token &token) {
                     || (token.value.find('e') != std::string::npos)
                     || (token.value.find('E') != std::string::npos);
 
+        bool isOct = (token.value.find('o') != std::string::npos)
+                  || (token.value.find('O') != std::string::npos);
+
+        bool isHex = (token.value.find('x') != std::string::npos)
+                  || (token.value.find('X') != std::string::npos);
+
         if (isFloat) {
             return VAR(Float, std::stod(token.value));
         }
         else {
-            return VAR(Int, std::stod(token.value));
+            int base = isOct ? 8 : isHex ? 16 : 10;
+
+            // remove 0x / 0o prefix if needed
+            auto str_value = base == 10 ? token.value : token.value.substr(2);
+
+            return VAR(Int, std::stoull(str_value, nullptr, base));
         }
     }
     default:
---
+++ test/test_python_progs/print_numbers.py
@@ -0,0 +1,16 @@
+print(-1)
+print(-123)
+print(-123.456)
+
+print(123456e-01)
+print(123_4_56e-0_1)
+
+print(1000000000000000)
+print(1_000_000_000_000_000)
+
+print(0o123456)
+print(0o123_456)
+
+print(0x5A3F)
+print(0xFFFFFFFF)
+print(0x_FF_FF_FF_FF)
